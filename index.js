8da4650bfb4aa330ce068e15240662bb018ed2ec
AndressaSRodrigues <srandressa2@gmail.com>
2023-07-05 17:23:32 -0400

    File and Directory Operations - tests

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+
+const fs = require('fs');
+const path = require('path');
+
+//Read a file
+fs.readFile('testing_files/testing-links.md', 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data);
+});
+
+//Extension
+const filePath = 'testing_files/testing.html';
+const extension = path.extname(filePath);
+console.log(extension);
+
+//Read directory synchronous
+const files = fs.readdirSync('testing_files');
+console.log(files);
+
+//Join paths
+const join = path.join('testing_files', 'testing-links.md');
+console.log(join);
---
+++ testing_files/testing-links.md
@@ -0,0 +1,6 @@
+This is a test file.
+
+[Node.js](https://nodejs.org/)
+[npm](https://www.npmjs.com/)
+[JavaScript Info](https://javascript.info/)
+[File System | Node](https://nodejs.org/api/fs) -- broken link example
